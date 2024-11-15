dc6e4d761fceed44e38e4709ac38c79b1c7a965a
Ionuț Constantin <122406111+ionutconstantin-dev@users.noreply.github.com>
2024-11-15 20:55:40 +0200

    Add files via upload

---
+++ FootballApp/src/test/java/com/football/FootballApp/FootballAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.FootballApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
